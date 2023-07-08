300a42d1147162386d20101eb0f5b951387d1312
ShuvoMallick01 <shuvacse01@gmail.com>
2023-07-08 16:16:30 +0600

    Complete Navbar and Banner section with responsive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-// === TITLE ===
-// === / TITLE ===
+// === NAVBAR ===
+const navbar = document.querySelector(".navbar");
+
+window.onscroll = () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add("scroll-nav-color");
+  } else {
+    navbar.classList.remove("scroll-nav-color");
+  }
+};
+
+// === / NAVBAR ===
 
 // === TITLE ===
 // === / TITLE ===
